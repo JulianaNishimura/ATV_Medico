9c9d6bba7b9bb894ae4d1204e7ded4a5b73e5512
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-07 14:14:36 -0300

    pastas

---
+++ ATV_Medico_Back/src/main/java/com/example/ATV_Medico_Back/AtvMedicoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ATV_Medico_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvMedicoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvMedicoBackApplication.class, args);
+	}
+
+}
---
+++ ATV_Medico_Back/src/test/java/com/example/ATV_Medico_Back/AtvMedicoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ATV_Medico_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvMedicoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
