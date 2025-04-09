e994c5839d224ef76d83d9bf3865672c90c627e6
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-09 13:43:28 -0300

    salvando

--- ATV_Medico_Back/src/main/java/com/example/ATV_Medico_Back/repository/MedicoRepository.java
+++ ATV_Medico_Back/src/main/java/com/example/ATV_Medico_Back/repository/MedicoRepository.java
@@ -9,9 +9,5 @@ import java.util.List;
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
-    List<Medico> findByNumeroConsultorio(Integer numeroConsultorio);
-    List<Medico> findByEspecialidade(String especialidade);
-    List<Medico> findByNomeContainingIgnoreCase(String nome);
-    // Novo método: buscar médico por CRM
     Medico findByCrm(String crm);
 }
